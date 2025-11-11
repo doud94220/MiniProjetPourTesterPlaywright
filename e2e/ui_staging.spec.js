8dcb96a207fb0dbebe91571408504a39991094c4
doud94220 <edouard.anthony@gmail.com>
2025-11-11 11:52:49 +0100

    Ajout console.log() partie UI Staging Tests

--- e2e/ui_staging.spec.js
+++ e2e/ui_staging.spec.js
@@ -5,9 +5,11 @@ const { test, expect } = require('@playwright/test');
 test('Vérifier que la navigation utilise l\'URL Staging (Herokuapp)', async ({ page }) => {
     // page.goto('/') va maintenant naviguer vers https://the-internet.herokuapp.com/
     await page.goto('/');
+    console.log("On est sur la page https://the-internet.herokuapp.com");
 
     // Le titre doit contenir le nom du site
     await expect(page).toHaveTitle('The Internet');
+    console.log("Le titre contient bien 'The Internet'");
 
     /*
         Vérifier un élément de la page :
@@ -15,4 +17,5 @@ test('Vérifier que la navigation utilise l\'URL Staging (Herokuapp)', async ({ 
             exact: false: On dit à Playwright que d'autres caractères peuvent suivre le mot "Welcome" (to the-internet, par exemple).
     */
     await expect(page.getByRole('heading', { name: 'Welcome', exact: false })).toBeVisible();
+    console.log("La page contient bien 'Welcome'");
 });
